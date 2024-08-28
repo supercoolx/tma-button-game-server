efb7b3f701608317ddf0ea508145a7094346dc74
aaron927dev <star.aaron.dev@gmail.com>
2024-08-28 08:07:45 -0400

    fix: change authController

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,19 +4,11 @@ const router = express.Router();
 const { authenticateUser } = require('../middleware/authentication');
 
 const {
-  register,
   login,
   logout,
-  verifyEmail,
-  forgotPassword,
-  resetPassword,
 } = require('../controllers/authController');
 
-router.post('/register', register);
 router.post('/login', login);
 router.delete('/logout', authenticateUser, logout);
-router.post('/verify-email', verifyEmail);
-router.post('/reset-password', resetPassword);
-router.post('/forgot-password', forgotPassword);
 
 module.exports = router;
