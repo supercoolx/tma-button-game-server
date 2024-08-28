afff12d048023d4cbce59c7334fafa300857ddfb
supercoolx <94512319+supercoolx@users.noreply.github.com>
2024-08-28 09:07:03 -0700

    fix: model email unique

--- models/User.js
+++ models/User.js
@@ -18,7 +18,6 @@ const UserSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    unique: true,
     required: [true, 'Please provide email'],
     validate: {
       validator: validator.isEmail,
