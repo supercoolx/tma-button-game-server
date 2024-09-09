1691deea6cce7639b9b6e8b50efd4beb886f88c4
eugene715 <eugene0715@gmail.com>
2024-09-09 19:50:11 +0000

    fix: jackpot

--- helper/botHelper.js
+++ helper/botHelper.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-const { Bot, InlineKeyboard } = require("grammy");
-
 const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.BOT_TOKEN); // tracker bot @button_coin_tracker_bot
@@ -56,6 +54,8 @@ module.exports.sendMessageToAdmins = (text, channelId = CHANNEL_ID) => bot.getCh
 
 module.exports.botStart = () => {
     try {
+        
+        const { Bot, InlineKeyboard } = require("grammy");
         const gameBot = new Bot(process.env.BOT_TOKEN);
 
         gameBot.command('start', async (ctx) => {
